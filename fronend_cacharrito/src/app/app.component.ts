86616aea1476f88c2812add3ef38c48a5def68ce
Valegg01 <valentinagomezg75@gmail.com>
2025-03-15 15:08:46 -0500

    refactorizacion del frontend

--- fronend_cacharrito/src/app/app.component.ts
+++ fronend_cacharrito/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LoginAdministradorComponent } from "./login-administrador/login-administrador.component";
-import { NavComponent } from "./nav/nav.component";
-import { SignInComponent } from './sign-in/sign-in.component';
+import { LoginAdministradorComponent } from "./administrador/login-administrador.component";
+import { NavComponent } from "./Principal/nav.component";
+import { SignInComponent } from './Registro-Usuario/sign-in.component';
 
 @Component({
   selector: 'app-root',
