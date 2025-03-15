b371fb360fc7b64b3c34f2ca8dba7e744bb9017f
Valegg01 <valentinagomezg75@gmail.com>
2025-03-15 17:51:48 -0500

    cambios en rutas y subida de imagne

--- fronend_cacharrito/src/app/app.routes.ts
+++ fronend_cacharrito/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from "@angular/router";
 import { LoginAdministradorComponent } from "./administrador/login-administrador.component";
-import { SignInComponent} from "./Registro-Usuario/sign-in.component"
+import { RegistroUsuarioComponent } from "./registro-usuario/registro-usuario.component";
 
 
 export const routes: Routes = [
     {path: 'administrador', component: LoginAdministradorComponent},
-    {path: 'iniciarSesion', component: SignInComponent}
+    {path: 'registro', component: RegistroUsuarioComponent}
 ];
 
-
+export class AppRoutingModule{}
 
 
     
