0bd0ea21248f4b3d0b0fb1290687337efcfb78b6
Valegg01 <valentinagomezg75@gmail.com>
2025-03-16 22:28:09 -0500

    respresentacion de coches con disponibilidad en el componente usuario

--- nuevo-frontend/src/app/usuario/usuario.component.ts
+++ nuevo-frontend/src/app/usuario/usuario.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Coche } from '../entidades/coche';
+import { CocheService } from '../servicios/coche.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
@@ -11,6 +15,12 @@ export class UsuarioComponent implements OnInit {
   ngOnInit(): void {
     this.ocultar_loguin('none')
   }
+
+  constructor(private servicioCoche: CocheService){}
+
+  coches!: Coche[];
+
+  tipo!: string;
 
 
   ocultar_loguin(valor:string){
@@ -20,5 +30,24 @@ export class UsuarioComponent implements OnInit {
     prin!.style.display=`${valor}`;
 
   }
+
+  ver_coches(){
+    this.servicioCoche.vehiculos_disponibles(this.tipo).subscribe(dato =>{
+
+      if(dato != false){
+
+        this.coches=dato;
+
+      }else{
+        alert("no se encuentran coches en la categoria "+ this.tipo)
+      }
+
+    },error =>{
+      alert(" error de respuesta, intentelo mas tarde")
+      console.log(error)
+    })
+  }
+
+
 
 }
