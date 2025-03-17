edcf540b10db236670ab32f9a2b60b1d466ff5b2
Valegg01 <valentinagomezg75@gmail.com>
2025-03-17 11:49:32 -0500

    cambios en usuario

--- nuevo-frontend/src/app/usuario/usuario.component.ts
+++ nuevo-frontend/src/app/usuario/usuario.component.ts
@@ -1,13 +1,15 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Coche } from '../entidades/coche';
 import { CocheService } from '../servicios/coche.service';
 import { FormsModule } from '@angular/forms';
+import { differenceInDays } from 'date-fns'; 
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
   imports: [CommonModule, FormsModule],
+  providers:[DatePipe],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
@@ -16,11 +18,21 @@ export class UsuarioComponent implements OnInit {
     this.ocultar_loguin('none')
   }
 
-  constructor(private servicioCoche: CocheService){}
+  constructor(private servicioCoche: CocheService, private datepipe:DatePipe){}
 
   coches!: Coche[];
 
+  coche: Coche = new Coche();
+
   tipo!: string;
+
+  finicio!:string;
+  nfinicio!:string;
+
+  ffin!:string;
+  nffin!:string;
+
+  daysDifference: number | null = null;
 
 
   ocultar_loguin(valor:string){
@@ -46,6 +58,40 @@ export class UsuarioComponent implements OnInit {
       alert(" error de respuesta, intentelo mas tarde")
       console.log(error)
     })
+  }
+
+  formatearfecha(){
+
+    this.nfinicio = this.datepipe.transform(this.finicio,'dd/MM/yyy')||'';
+    this.nffin = this.datepipe.transform(this.ffin,'dd/MM/yyy')||'';
+ 
+  }
+
+  calcular_dias(){
+
+    if (this.finicio && this.ffin) {
+      const start = new Date(this.finicio);
+      const end = new Date(this.ffin);
+      this.daysDifference = differenceInDays(end, start);
+    } else {
+      this.daysDifference = null;
+    }
+
+  }
+
+
+  alquilar_coche(id:string){
+
+    const encontrado = this.coches.find(buscado => buscado.placa === id);
+   
+
+    if (encontrado) {
+      this.coche = encontrado;
+    } else {
+      console.error("Coche no encontrado con la placa:", id);
+    }
+    
+
   }
 
 
